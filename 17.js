4dd71d261aa0daa77f23a0039bad8dc08e9c2b66
saadhn4 <saadcfi4@gmail.com>
2024-11-06 21:37:00 +0530

    added more function methods

--- 17.js
+++ 17.js
@@ -282,3 +282,211 @@ function sqtable() {
   }
 }
 sqtable();
+
+//arrow functions
+
+//1. arrow functions were introduced in ES6(2015)
+//2. arrow functions allow us to write shorter function syntax
+
+//Before arrow functions
+
+//1.
+// function sayHello() {
+//   console.log("Hello");
+// }
+// sayHello();
+
+//2.
+// let sayHell0 = function () {
+//   console.log("Hello");
+// };
+// sayHello();
+
+//Arrow function example:
+
+let sayHell0 = () => {
+  console.log("Hello");
+};
+sayHell0();
+
+console.log("------------------");
+
+//*it is pretty similar to function expression except there is no function keyword and a => after the parantheses.*
+
+//lets try doing sum of 2 numbers in arrow
+//in arrow, if your curly braces only has 1 line like return a + b; u dont need curly braces or the return keyword
+//you can use curly braces tho still
+
+let sumofNumbers = (a, b) => {
+  return a + b;
+};
+console.log(sumofNumbers(7, 2));
+
+console.log("------------------");
+
+//demonstration of what i was talking ab in line 319
+
+let example = (a, b) => a + b;
+console.log(example(1, 3));
+
+console.log("------------------");
+
+//Hoisting
+
+/*
+
+Hoisting is a mechanism where variable and function declarations are moved ("or hoisted")
+Hoisting only works in a simple function (not function expression and arrow)
+to the top of their scope before code execution.
+This means, you can use variables and functions before declaring them in the code.
+However, note that onlt declarations are hoisted, not initializations or assignment.
+In JS, you can call the function even before it is already declared
+
+*/
+
+test(); //instead of calling the function after curly braces, im calling it before. will print hello
+
+function test() {
+  console.log("Hello");
+}
+//now if i still were to declare it after it would still work. will print hello
+test();
+
+console.log("------------------");
+
+//NOTE------*With arrow functions and function expression, we cannot use hoisting.*
+//this holds true even if we were to declare the function with var.
+
+//lets try doing it in arrow function
+// testt();
+// let testt = () => {
+//   console.log("Heuuuiiuu");
+// };
+// testt(); //ReferenceError: Cannot access 'testt' before initialization
+
+//Anonymous function
+
+/*
+
+An anonymous function is a function that does not have a name
+Its often used when you need a function as a value
+Particularly in cases where where you don't need to  reference the function later.
+
+anonymous functions are useful for single-use function or for functions 
+passed as argument.
+
+It is declared same as function expression
+
+*/
+
+let sayhello = function () {
+  console.log("hello");
+};
+
+//IIFE (immediately Invoked function expression
+(function () {
+  console.log("IIFE executed immediately");
+})();
+
+console.log("------------------");
+
+//Recursion
+
+//It is a process in which a function calls itself directly or indirectly in order to solve a problem
+//by breaking it down into smaller, similar sub problems.
+//This repeated self-calling contunues until it reaches a base case
+//which is a condition where no further recursion is needed
+//allowing the function to start returning values back up through each call
+
+//in simpler terms
+//recursion is when something is defined or done in terms of itself until it reaches a stopping point
+
+//example
+
+// function call(number) {
+//   console.log(number);
+//   //call(6); it gives me an infinite loop of 6
+// }
+// call(6);
+
+//now what if i called the 6 inside the code block itself? (check line 412) (dont do this XD)
+
+//example in class:
+//using recursion to print 5-0
+
+// function call(num) {
+//   if (num <= 0) {
+//     //it runs a backwards loop and when it reaches 0 ; the condition is true. so it will execcute the "loop has stopped" statement
+//     return console.log(" the loop has stopped");
+//   }
+//   console.log(num);
+//   call(num - 1); //infinite loop of 7 so it goes like 5 4 3 2 1 ; nnow the condition is true ; so loop has stopped prints.
+// }
+// call(5);
+
+// console.log("------------------");
+
+//ill try again lets print from 100-10
+
+function call(num) {
+  if (num <= 9) {
+    return console.log("stop that shit"); //value ku stop karne return zaroori hai. to end the if block
+  }
+  console.log(num);
+  call(num - 1); //this runs infinite loop. 100 99 98 ..... 10 ; now 9<=9 is true and the stop stm prints.
+}
+call(100); //this calls 100
+
+console.log("------------------");
+
+//lets try factorial
+
+function factorial(number) {
+  if (number == 0 || number == 1) {
+    return 1;
+  }
+  return number * factorial(number - 1); //line 454's function is being called here and were making changes to it
+}
+console.log(factorial(5));
+
+console.log("------------------");
+
+//factorial of 2
+
+function fac(num) {
+  if (num == 1 || num == 0) {
+    return 1;
+  }
+  return num * fac(num - 1);
+}
+console.log(fac(2));
+
+console.log("------------------");
+
+//print numbers from 10-8
+
+function print(numbers) {
+  if (numbers <= 7) {
+    return console.log("Stop");
+  }
+  console.log(numbers);
+  print(numbers - 1);
+}
+print(10);
+
+console.log("------------------");
+
+//more recursion practice
+
+let num = 1;
+function show() {
+  if (num >= 6) {
+    return console.log("Stop");
+  }
+  console.log("Hi", num);
+  num++;
+  show();
+}
+show();
+
+console.log("------------------");
